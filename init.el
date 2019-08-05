9af50483da553e84a468fbea661099ab266737db
Ethan Kuefner <eakuefner@google.com>
2019-08-04 20:16:08 -0400

    add more logic

--- init.el
+++ init.el
@@ -4,3 +4,16 @@
 
 (require 'evil)
 (evil-mode 1)
+
+(setq base16-theme-256-color-source "base16-shell")
+(load-theme 'base16-default-light t)
+
+(global-display-line-numbers-mode)
+
+(add-to-list 'default-frame-alist
+             '(font . "Anonymous Pro:pixelsize=16:antialias=true:autohint=true"))
+
+(require 'org)
+(define-key global-map "\C-cl" 'org-store-link)
+(define-key global-map "\C-ca" 'org-agenda)
+(setq org-log-done t)
