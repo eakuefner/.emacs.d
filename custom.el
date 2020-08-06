52990af47fd0a3b247b09b7684b331f5cf2bd736
Ethan Kuefner <eakuefner@gmail.com>
2020-08-05 21:47:10 -0400

    Install magit
    Another external-facing oversight; magit isn't included with emacs
    for Windows.

--- custom.el
+++ custom.el
@@ -6,7 +6,7 @@
  '(custom-safe-themes
    (quote
     ("2a998a3b66a0a6068bcb8b53cd3b519d230dd1527b07232e54c8b9d84061d48d" "16dd114a84d0aeccc5ad6fd64752a11ea2e841e3853234f19dc02a7b91f5d661" default)))
- '(package-selected-packages (quote (haskell-mode base16-theme evil))))
+ '(package-selected-packages (quote (magit haskell-mode base16-theme evil))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
